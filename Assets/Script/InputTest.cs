13dc9e081e9b4aa0bf7bf9da5b22d6ec418e8d99
Romain Rabouan <romain.rabouan1@gmail.com>
2021-11-29 18:54:35 +0100

    Initial commit

---
+++ Assets/Script/InputTest.cs
@@ -0,0 +1,44 @@
+
+
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputTest : MonoBehaviour
+{
+    private Gamepad controller = null;
+    private Transform m_transform;
+
+    void Start()
+    {
+        this.controller = DS4.getConroller();
+        m_transform = this.transform;
+    }
+
+    void Update()
+    {
+        if (controller == null)
+        {
+            try
+            {
+                controller = DS4.getConroller();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+        else
+        {
+            // Press circle button to reset rotation
+            if (controller.buttonEast.isPressed)
+            {
+                m_transform.rotation = Quaternion.identity;
+            }
+            m_transform.rotation *= DS4.getRotation(4000 * Time.deltaTime);
+
+            /// Acceleration
+       //     m_transform.Translate(DS4.GetAcceleration(20 * Time.deltaTime));
+        }
+    }
+}
